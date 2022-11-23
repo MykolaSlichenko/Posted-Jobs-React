3ebb62fd40067ad067fd94cc5f4d61a3a42565c9
dell <nikolaiiislichenko@gmail.com>
2022-11-23 16:52:59 +0200

    click on all area redirect to job

--- src/components/JobsPage.js
+++ src/components/JobsPage.js
@@ -10,21 +10,22 @@ export function JobsPage() {
   return (
     <div className='jobPageStyle'>
       {jobs.map((job, index) => (
-        <div className='card' key={index}>
-          {console.log(index)}
-          <div className='img-part'>
-            <img className='image' src={job.image} />
+        <Link className='link' to={`/jobs/${job.id}`} exact>
+          <div className='card' key={index}>
+            {console.log(index)}
+            <div className='img-part'>
+              <img className='image' src={job.image} />
+            </div>
+            <div className='text-part'>
+              <h3 className='title'>{job.title}</h3>
+              <p className='card-text'>{job.text}</p>
+              <p className='location-style'>{job.location}</p>
+              <p className='time'>{job.time}</p>
+              <img className='img-card' src={require('../images/Rectangle.png')} />
+            </div>
           </div>
-          <div className='text-part'>
-            {/*TODO click on all area redirect to job*/}
-            <h3 className='title'><Link className='link' to={`/jobs/${job.id}`} exact>{job.title}</Link></h3>
-            <p className='card-text'>{job.text}</p>
-            <p className='location-style'>{job.location}</p>
-            <p className='time'>{job.time}</p>
-            <img className='img-card' src={require('../images/Rectangle.png')} />
-          </div>
-        </div>
-      ))}
-    </div>
+          </Link>
+        ))}
+      </div>
   );
 }
